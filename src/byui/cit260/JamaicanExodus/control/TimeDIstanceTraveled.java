71a472484e2225ba1a0872ff93d3c2f4457aef99
mikeg <mikeg@Mikes>
2017-10-18 21:25:51 -0700

    Merge origin/master
    Conflicts:
    	src/byui/cit260/JamaicanExodus/control/ResourceShopControl.java
    	test/byui/cit260/JamaicanExodus/control/InventoryControlTest.java
    	test/byui/cit260/JamaicanExodus/control/ResourceShopControlTest.java

---
+++ src/byui/cit260/JamaicanExodus/control/TimeDIstanceTraveled.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JamaicanExodus.control;
+
+/**
+ *
+ * @author mikeg
+ */
+public class TimeDIstanceTraveled {
+   
+    
+    public class timeTraveled(speed, distance){
+      
+        if(speed < 0)
+            THEN ("Invlaid Speed");
+        
+    }
+    
+}
