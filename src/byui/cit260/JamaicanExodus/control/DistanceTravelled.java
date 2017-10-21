05ddbfcbe4435fe2e186c6bc722c73fef49b9348
mikeg <mikeg@Mikes>
2017-10-21 13:10:06 -0700

    Merge origin/master
    Conflicts:
    	src/byui/cit260/JamaicanExodus/control/ResourceShopControl.java
    	test/byui/cit260/JamaicanExodus/control/InventoryControlTest.java
    	test/byui/cit260/JamaicanExodus/control/ResourceShopControlTest.java

---
+++ src/byui/cit260/JamaicanExodus/control/DistanceTravelled.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JamaicanExodus.control;
+
+/**
+ *
+ * @author mikeg
+ */
+
+import java.util.Scanner;
+
+// Distance = Speed * Time
+public class DistanceTravelled {
+    public static void main( String [] arg) {
+        Scanner scanner = new Scanner( System.in) ;
+        double travelingSpeed;
+        int timeTravelled;
+        
+        System.out.println( "Please enter your speed");
+        travelingSpeed = scanner.nextDouble();
+        
+        
+        for( int hour = 1; hour <= travelingSpeed; hour++ ){
+        
+        }
+    }
+}
