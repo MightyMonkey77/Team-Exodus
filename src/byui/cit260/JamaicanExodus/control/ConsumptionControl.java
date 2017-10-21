58e9223a68aa2348728490be85ae9dd0d8b4cc17
mikeg <mikeg@Mikes>
2017-10-21 14:28:34 -0700

    Merge origin/master
    Conflicts:
    	src/byui/cit260/JamaicanExodus/control/ResourceShopControl.java
    	test/byui/cit260/JamaicanExodus/control/InventoryControlTest.java
    	test/byui/cit260/JamaicanExodus/control/ResourceShopControlTest.java

--- src/byui/cit260/JamaicanExodus/control/ConsumptionControl.java
+++ src/byui/cit260/JamaicanExodus/control/ConsumptionControl.java
@@ -7,8 +7,62 @@ package byui.cit260.JamaicanExodus.control;
 
 /**
  *
- * @author Office Payne
+ * @author Kim Haereiti
  */
 public class ConsumptionControl {
     
+        
+    public double consumptionRate(int pace, int distance) {
+        
+        double paceImpact = 50;
+        double consumed = 0;
+
+        if (pace < 0) 
+        {
+            System.out.println("\t'Error: Minimum pace is 0.'");
+            return 0.00;
+        }
+        else if(pace == 0) 
+        {
+            return .50;
+        }
+        else if(pace == 1) 
+        {
+            paceImpact = 1.10;
+        }
+        else if(pace == 2) 
+        {
+            paceImpact = 1.35;
+        }
+        else if(pace == 3) 
+        {
+            paceImpact = 1.60;
+        }
+        else if(pace == 4) 
+        {
+            paceImpact = 2.00;
+        }
+        else 
+        {
+            System.out.println("\t'Error: Maximum pace is 4.'");
+            return 0.00;
+        }
+        if (distance < 0) 
+        {
+            System.out.println("\t'Error: Minimum distance is 0.'");
+            return 0.00;
+        }
+        else if (distance == 0)
+        {
+            return .50;
+        }
+        else if (distance > 201) 
+        {
+            System.out.println("\t'Error: Maximum distance per day is 200.'");
+            return 0.00;
+        }
+        consumed = distance * paceImpact / 100;
+        return consumed;
 }
+} 
+
