6c5d1118c545ffb4e0da28e0240120440cfe9959
mikeg <mikeg@Mikes>
2017-11-07 21:15:09 -0800

    Mike -
    Added Game Goal
    
    11/7/17

---
+++ src/byui/cit260/JamaicanExodus/view/GameGoal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JamaicanExodus.view;
+
+/**
+ *
+ * @author mikeg
+ */
+public abstract class GameGoal extends View{
+    
+}
+    
+    public GameGoal {
+       super(+"\n  - E exit"
+             +"\n");
+
+@orverride
