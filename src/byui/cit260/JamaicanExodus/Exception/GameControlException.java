157f5285ab9ba8e028bc444e156fdabb7ae3f1d0
mikeg <mikeg@Mikes>
2017-11-21 22:35:12 -0800

    added game control occupations 11/21/17 Mike

--- src/byui/cit260/JamaicanExodus/Exception/GameControlException.java
+++ src/byui/cit260/JamaicanExodus/Exception/GameControlException.java
@@ -11,8 +11,26 @@ package byui.cit260.JamaicanExodus.Exception;
  */
 public class GameControlException extends Exception {
 
-    public GameControlException(String message) {
+    public GameControlException() {
     }
+
+    public GameControlException(String string) {
+        super(string);
+    }
+
+    public GameControlException(String string, Throwable thrwbl) {
+        super(string, thrwbl);
+    }
+
+    public GameControlException(Throwable thrwbl) {
+        super(thrwbl);
+    }
+
+    public GameControlException(String string, Throwable thrwbl, boolean bln, boolean bln1) {
+        super(string, thrwbl, bln, bln1);
+    }
+
+ 
 
    
     
