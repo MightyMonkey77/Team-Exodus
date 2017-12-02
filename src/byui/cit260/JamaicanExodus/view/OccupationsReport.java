ccca6e9846f15a2ced14405b9a7a912f4efbaf23
mikeg <mikeg@Mikes>
2017-12-02 13:33:52 -0800

    Tried to added Occupations List  12/2/17 Mike

---
+++ src/byui/cit260/JamaicanExodus/view/OccupationsReport.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JamaicanExodus.view;
+
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+//import java.io.ObjectInputStream;
+//import java.io.ObjectOutputStream;
+
+/**
+ *
+ * @author mikeg
+ */
+public class OccupationsReport {
+    
+   public static void main(String[] args){
+      
+        //OccupationsList o1 = new Occupations();
+        //ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Occupations.txt"));
+        //out.writeObject(o1);
+        
+        //ObjectInputStream in = new ObjectInputStream(new FileInputStream("Occupations.txt"));
+        //OccupationsList o2 =(Occupations) in.readObject();
+        
+        //System.out.println(o2.getOccupations());
+        
+       File file = new File("OccupationsList.txt");
+       try {
+           PrintWriter output = new PrintWriter(file);
+           output.println("Banker, Carpenter, Farmer, Shipwright, Software Engineer");
+           output.close();
+       } catch (IOException ex) {
+           System.out.printf("ERROR: %s\n ", ex);
+        
+        }
+
+   }
+}
