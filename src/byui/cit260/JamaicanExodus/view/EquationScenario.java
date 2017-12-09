a1dcebe36448e36150ce5c5a7570c6c5f31b99fe
mikeg <mikeg@Mikes>
2017-12-09 13:13:01 -0800

    Added Equation Scenario 12/9/17 Mike

---
+++ src/byui/cit260/JamaicanExodus/view/EquationScenario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.JamaicanExodus.view;
+
+/**
+ *
+ * @author mikeg
+ */
+public class EquationScenario implements Scenario {
+    
+    //public int ();
+        //System.out.println("Put your thinking cap on, time for math");
+        //System.out.
+    
+}
